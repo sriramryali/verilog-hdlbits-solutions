445359f148b11e84ec2e077cebc733dc452778a6
sriramryali <sriramryali01@gmail.com>
2025-08-17 12:05:30 +0530

    first-commit

--- hdlbits-solutions/02-verilog-language/01-basics/03_inverter.v
+++ hdlbits-solutions/02-verilog-language/01-basics/03_inverter.v
@@ -1,8 +1,8 @@
-// Basic implemention an inverter
+// Basic implementation an inverter
 module top_module(
     input in,
     output out);
-    assign out = in; // This is a behavioral design style 
+    assign out = ~in; // This is a behavioral design style, using out = !in is not correct, because ! is used for logical or boolean operation, simply true (any non zero value) or false (all bits are zero)
 endmodule
 
 
--- hdlbits-solutions/02-verilog-language/01-basics/05_nor_gate.v
+++ hdlbits-solutions/02-verilog-language/01-basics/05_nor_gate.v
@@ -10,3 +10,8 @@ endmodule
 module top_module(input a,b output out);
     nor #10 S(out,a,b); // Structural or Netlist description, #10 defines the delay of the gate
 endmodule
+
+
+// Binary vs Boolean: 
+// Binary -> ~,|,& these are all bit wise operations, used in hardware gate implementations
+// Boolean or logical -> !,||,&& these are all boolean or logical operators, used in logical expressions (true or false)
