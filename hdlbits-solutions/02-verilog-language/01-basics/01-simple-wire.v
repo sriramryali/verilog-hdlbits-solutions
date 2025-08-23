98d58216a1f4356f026a020fa6cc1aa5b5ee503c
sriramryali <sriramryali01@gmail.com>
2025-08-23 17:50:57 +0530

    first-commit

--- hdlbits-solutions/02-verilog-language/01-basics/01-simple-wire.v
+++
@@ -1,4 +0,0 @@
-// A simple buffer (out = in), with no delay
-module top_module(output out,input in);
-    assign out = in; // a simple buffer
-endmodule
---
+++ hdlbits-solutions/02-verilog-language/02-vectors/01_vectors.v
@@ -0,0 +1,12 @@
+// Basic implementation of vectors
+module top_module(
+    input [2:0] vec,      // wire [msb : lsb] name, the size of the vector is mentioned before its name, ex: reg [0:3] latch -> a 4-bit vector, 0 -> MSB 3-> LSB 
+    output [2:0] outv,    // slicing(taking a part of the vector) can be done, this comes after thr name, ex: latch[1:2]
+    output o0,            // Once a vector is defined, it MSB and LSB cannot be changed, ex: assign mux = latch[3:0] -> wrong, because latch is from 0 to 3 (latch[0:3])
+    output o1,
+    output o2);
+    assign outv = vec;
+    assign o0 = vec[0];
+    assign o1 = vec[1];
+    assign o2 = vec[2];
+endmodule
