57b06c271478ca76844a9338c2ca63c76545e310
sriramryali <sriramryali01@gmail.com>
2025-08-23 23:38:04 +0530

    first-commit

--- hdlbits-solutions/02-verilog-language/02-vectors/01_vectors.v
+++ hdlbits-solutions/02-verilog-language/02-vectors/01_vectors.v
@@ -1,6 +1,6 @@
 // Basic implementation of vectors
 module top_module(
-    input [2:0] vec,      // wire [msb : lsb] name, the size of the vector is mentioned before its name, ex: reg [0:3] latch -> a 4-bit vector, 0 -> MSB 3-> LSB, -ve ranges are also allowed
+    input [2:0] vec,      // type [msb : lsb] name. Type can be wire, reg, input and output, size of the vector is mentioned before its name. ex: reg [0:3] latch -> a 4-bit vector, 0 -> MSB 3-> LSB, -ve ranges are also allowed, [0:0] -> single bit vector
     output [2:0] outv,    // slicing(taking a part of the vector) can be done, this comes after the name, ex: latch[1:2]
     output o0,            // Once a vector is defined, it MSB and LSB cannot be changed, ex: assign mux = latch[3:0] -> wrong, because latch is from 0 to 3 (latch[0:3])
     output o1,
