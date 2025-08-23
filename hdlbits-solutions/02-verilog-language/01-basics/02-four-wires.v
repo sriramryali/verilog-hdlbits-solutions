67a9c2a3b22eed7771fe650403071e31a42577b7
sriramryali <sriramryali01@gmail.com>
2025-08-23 17:54:13 +0530

    Delete hdlbits-solutions/02-verilog-language/01-basics/02-four-wires.v

--- hdlbits-solutions/02-verilog-language/01-basics/02-four-wires.v
+++
@@ -1,7 +0,0 @@
-// A combinational ckt with four wires
-module top_module(input a,b,c, output w,x,y,z);
-    assign w = a; // connecting outputs to corresponding inputs
-    assign x = b; 
-    assign y = b;
-    assign z = c;
-endmodule
