a0a90d05c260118eb26b69bba0b365f49b0bcaed
sriramryali <sriramryali01@gmail.com>
2025-08-24 13:09:44 +0530

    first-commit

---
+++ hdlbits-solutions/02-verilog-language/02-vectors/09_more_replication.v
@@ -0,0 +1,6 @@
+// A problem on replication
+module top_module(
+    input a,b,c,d,e;
+    output out);
+    assign out = ~{{5{a}},{5{b}},{5{c}},{5{d}},{5{e}}} ^ {5{a,b,c,d,e}};  // using xnor gate to compare two bits, output 1 if equal
+endmodule
