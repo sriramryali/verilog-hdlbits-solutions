683e3663c3e75e3c7875585c5442ce73108e9f8d
sriramryali <sriramryali01@gmail.com>
2025-08-24 22:34:50 +0530

    first-commit

---
+++ hdlbits-solutions/02-verilog-language/03-modules-hierarchy/01_modules.v
@@ -0,0 +1,15 @@
+// Instantiating modules by name 
+module top_module(
+    input a,
+    input b,
+    output out);
+    mod_a example(.in1(a), .in2(b), .out(out));  // connecting modules by name, verbose but reduces errors and can be written in any order
+endmodule
+
+// Instantiating modules by position
+module top_module(
+    input a,
+    input b,
+    output out);
+    mod_a example(a,b,out);   // connecting modules by position, should be written in order, less verbose but errors might occur if not in order
+endmodule
