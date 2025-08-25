20bea71939787baa1f7283234708a98def7eb14e
sriramryali <sriramryali01@gmail.com>
2025-08-25 10:46:46 +0530

    vectors-done

--- hdlbits-solutions/02-verilog-language/02-vectors/09_more_replication.v
+++ hdlbits-solutions/02-verilog-language/02-vectors/09_more_replication.v
@@ -3,4 +3,4 @@ module top_module(
     input a,b,c,d,e;
     output out);
     assign out = ~{{5{a}},{5{b}},{5{c}},{5{d}},{5{e}}} ^ {5{a,b,c,d,e}};  // using xnor gate to compare two bits, output 1 if equal
-endmodule
+endmodule 
