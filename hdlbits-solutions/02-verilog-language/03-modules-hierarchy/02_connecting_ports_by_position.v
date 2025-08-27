d3737547fb8d9fbcdafcdd719b7c4628e5a5eb93
sriramryali <sriramryali01@gmail.com>
2025-08-27 23:00:17 +0530

    first-commit

--- hdlbits-solutions/02-verilog-language/03-modules-hierarchy/02_connecting_ports_by_position.v
+++ hdlbits-solutions/02-verilog-language/03-modules-hierarchy/02_connecting_ports_by_position.v
@@ -3,5 +3,13 @@ module top_module(
     input a,b,c,d,
     output out1,
     output out2);
-    mod_a example(out1,out2,a,b,c,d);   // connected ports of mosules by position
+    mod_a example(out1,out2,a,b,c,d);   // should be in the same order as that of instantiated module
 endmodule 
+
+// mod_a
+module mod_a(
+    input in1,in2,in3,in4,
+    output out1,
+    output out2);
+    // body
+endmodule
