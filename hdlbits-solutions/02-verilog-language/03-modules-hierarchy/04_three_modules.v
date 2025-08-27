aa533fd95cd392c2edc1a4b1a0189582fe183034
sriramryali <sriramryali01@gmail.com>
2025-08-27 23:17:48 +0530

    first-commit

---
+++ hdlbits-solutions/02-verilog-language/03-modules-hierarchy/04_three_modules.v
@@ -0,0 +1,10 @@
+// Instantiating D_FFs in a counter module
+module top_module(
+    input clk,
+    input d,
+    output q);
+    wire t1,t2;     // for the outputs of first two d_ffs
+    d_ff inst1(.clk(clk), .d(d), .q(t1));     // simply once see the ckt diagram, then coding will be super easy
+    d_ff inst2(.clk(clk), .d(t1), .q(t2));
+    d_ff inst3(.clk(clk), .d(t2), .q(q));
+endmodule
