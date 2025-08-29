3e52a3a063c4a2d8024de14155dcc98beb4cfbea
sriramryali <sriramryali01@gmail.com>
2025-08-29 23:14:22 +0530

    modules-done

--- hdlbits-solutions/02-verilog-language/03-modules-hierarchy/09_adder_subtractor.v
+++ hdlbits-solutions/02-verilog-language/03-modules-hierarchy/09_adder_subtractor.v
@@ -2,12 +2,12 @@
 module top_module(
     input [31:0] a,
     input [31:0] b,
-    input sub,
-    output [31:0] sum);
-    wire [15:0] b_not;
-    wire [15:0] sum1;               // The same ckt can be used for both addition and subtraction
-    wire [15:0] sum2;               // when sub = 0, cin for first adder is 0 and b_new = b, addition takes place as usually here
-    wire c1,c2;                     // when sub = 1, 2's complement of b(~b) is added with a and 1(sub here is 1), and hence the result is 2's CR(2's complement representation) of subtracted result
+    input sub,                     // The same ckt can be used for both addition and subtraction
+    output [31:0] sum);            // when sub = 0, cin for first adder is 0 and b_new = b, addition takes place as usually here
+    wire [15:0] b_not;             // when sub = 1, 2's complement of b(~b) is added with a and 1(sub here is 1), and hence the result is 2's CR(2's complement representation) of subtracted result
+    wire [15:0] sum1;               
+    wire [15:0] sum2;               
+    wire c1,c2;                     
     assign b_new = sub ? ~b : b;    // other way -> b_not = b ^ sub
     add16 inst1(.a(a[15:0]), .b(b_new[15:0]), .cin(sub), .cout(c1), .sum(sum1));    // this is for the lower 16 bits
     add16 inst2(.a(a[31:16]), .b(b_new[31:16]), .cin(c1), .cout(c2), .sum(sum2));   // this is for the upper 16 bits
