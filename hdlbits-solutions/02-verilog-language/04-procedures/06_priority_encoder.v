dca7f1073e857fdd259c25f919b6d1aa480f7788
sriramryali <sriramryali01@gmail.com>
2025-09-03 12:33:44 +0530

    first-commit

--- hdlbits-solutions/02-verilog-language/04-procedures/06_priority_encoder.v
+++ hdlbits-solutions/02-verilog-language/04-procedures/06_priority_encoder.v
@@ -23,3 +23,25 @@ module top_module(
         endcase
     end
 endmodule
+
+
+// Same implementation using casex or casez -> less verbose and easy to use
+module top_module(
+    input [3:0],
+    output reg [1:0] pos);
+    always @(*) begin
+        casez (in)
+            4'bzzz1 : pos = 0;     
+            4'bzz10 : pos = 1;     // this can also be written as 4'bzz1z : pos = 2; inplace of 0, z can be written
+            4'bz100 : pos = 2;     // instead of casez, casex can also be used and it results the same
+            4'b1000 : pos = 3;     
+            default : pos = 0;     // same as 4'b0000 : pos = 0
+        endcase
+    end
+endmodule
+
+
+// ways to write :
+// 4'bz1zz : pos = 2;
+// 4'bz100 : pos = 2;   better to put zeros, easier to understand and logical
+// 4'bx100 : pos = 2; 
