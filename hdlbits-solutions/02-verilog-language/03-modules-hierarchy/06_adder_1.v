4dc25596c85a45f5e4723d608bf41644cd712ab1
sriramryali <sriramryali01@gmail.com>
2025-09-03 15:31:52 +0530

    first-commit

--- hdlbits-solutions/02-verilog-language/03-modules-hierarchy/06_adder_1.v
+++ hdlbits-solutions/02-verilog-language/03-modules-hierarchy/06_adder_1.v
@@ -2,7 +2,7 @@
 module top_module(
     input [31:0] a,
     input [31:0] b,
-    output [31:0] out);      // once you draw the ckt diagram, it becomes easier to write the code
+    output [31:0] sum);      // once you draw the ckt diagram, it becomes easier to write the code
     wire c1,c2;              // 1-bit c1 and c2
     wire [15:0] sum1, sum2;  // 16-bit sum1 and sum2
     add16 inst1(.a(a[15:0]), .b(b[15:0]), .cin(0), .cout(c1), .sum(sum1));      // first 16-bit adder
