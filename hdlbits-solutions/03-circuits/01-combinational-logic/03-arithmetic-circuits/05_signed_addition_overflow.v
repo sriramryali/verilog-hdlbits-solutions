510e54a8c23106262731927757a5a1b01e3e530a
sriramryali <sriramryali01@gmail.com>
2025-10-09 14:53:48 +0530

    initial commit

--- hdlbits-solutions/03-circuits/01-combinational-logic/03-arithmetic-circuits/05_signed_addition_overflow.v
+++ hdlbits-solutions/03-circuits/01-combinational-logic/03-arithmetic-circuits/05_signed_addition_overflow.v
@@ -4,6 +4,6 @@ module top_module(
     input [7:0] b,
     output [7:0] s,
     output overflow);
-    assign s = a + b;
+    assign s = a + b;                                                  // overflow is defined in the case of signed numbers and carry out is defined in the case of unsigned numbers
     assign overflow = (a[7] & b[7] & ~s[7])|(~a[7] & ~b[7] & s[7]);    // overflow occurs when either sign(last) digits of a and b are 0 and c is 1 or the last digits of a and b are 1 and c is 0
 endmodule
