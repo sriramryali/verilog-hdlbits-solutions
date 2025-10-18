5408add93354a50b7021b0c843bb71d013083103
sriramryali <sriramryali01@gmail.com>
2025-10-18 14:37:48 +0530

    initial commit

---
+++ hdlbits-solutions/03-circuits/02-sequential-logic/01-latches-and-flipflops/01_D_flip_flop.v
@@ -0,0 +1,9 @@
+// Implementation of a D-flipflop
+module top_module(
+    input d,
+    input clk,                   // clks are used in sequential ckts
+    output reg q);               // output has to be a reg, because it is used inside an always block (if the condition is not met, it has to hold the previous value, hence reg type variable)
+    always @(posedge clk) begin
+        q <= d;                  // for a single statement, begin-end is not needed
+    end
+endmodule
