26357708342e8c01e9cd3a454ddc4d62a1d35d44
sriramryali <sriramryali01@gmail.com>
2025-10-27 11:45:52 +0530

    initial commit

---
+++ hdlbits-solutions/03-circuits/02-sequential-logic/01-latches-and-flipflops/09_DFF+gate.v
@@ -0,0 +1,9 @@
+// Implementation of a given sequential ckt (DFF with XOR gate)
+module top_module(
+    input in,
+    input clk,
+    output out);
+    always @(posedge clk) begin
+        out <= out ^ in;   // given in the ckt diagram 
+    end
+endmodule
