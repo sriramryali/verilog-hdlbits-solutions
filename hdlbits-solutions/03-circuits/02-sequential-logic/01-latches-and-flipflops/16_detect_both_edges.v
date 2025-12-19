9c5791698cefb07355924fa2708785eafa73e087
sriramryali <sriramryali01@gmail.com>
2025-12-20 00:03:32 +0530

    initial commit

--- hdlbits-solutions/03-circuits/02-sequential-logic/01-latches-and-flipflops/16_detect_both_edges.v
+++ hdlbits-solutions/03-circuits/02-sequential-logic/01-latches-and-flipflops/16_detect_both_edges.v
@@ -6,6 +6,6 @@ module top_module(
     reg [7:0] Q;
     always @(posedge clk) begin
         Q <= in;
-        anyedge <= in ^ Q;    // as we need to detect both 0 to 1 and 1 to 0 transitions, we consider both +ve and -ve edge detectors -> ~in & Q | in & ~Q : simply and xor operation 
+        anyedge <= in ^ Q;    // as we need to detect both 0 to 1 and 1 to 0 transitions, we consider both +ve and -ve edge detectors -> ~in & Q | in & ~Q : simply an xor operation 
     end
     
