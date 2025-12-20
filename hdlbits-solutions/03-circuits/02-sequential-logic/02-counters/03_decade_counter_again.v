85fca1a3eb3b6833c57397ca80499761a60bf29b
sriramryali <sriramryali01@gmail.com>
2025-12-20 19:46:21 +0530

    initial commit

---
+++ hdlbits-solutions/03-circuits/02-sequential-logic/02-counters/03_decade_counter_again.v
@@ -0,0 +1,14 @@
+// Implementation of a decade counter that counts from 1 to 10
+module top_module(
+    input clk,
+    input reset,              // active high synchronus reset
+    output [3:0] q);
+    always @(posedge clk) begin 
+        if (reset || (q == 4'd10)) begin 
+            q <= 4'd1;        // counts from 1 to 10, and resets to value 1(4'b0001)
+        end
+        else begin 
+            q <= q + 1;       // normal counter operation
+        end
+    end
+endmodule
