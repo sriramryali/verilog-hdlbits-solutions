f4135b004cc99d85d709c07e347604f02599b381
sriramryali <sriramryali01@gmail.com>
2025-12-20 11:57:38 +0530

    initial commit

--- hdlbits-solutions/03-circuits/02-sequential-logic/01-latches-and-flipflops/01_D_flip_flop.v
+++ hdlbits-solutions/03-circuits/02-sequential-logic/01-latches-and-flipflops/01_D_flip_flop.v
@@ -4,10 +4,11 @@ module top_module(
     input clk,                   // clks are used in sequential ckts
     output reg q);               // output has to be a reg, because it is used inside an always block (if the condition is not met, it has to hold the previous value, hence reg type variable)
     always @(posedge clk) begin
-        q <= d;                  // for a single statement, begin-end is not needed
+        q <= d;                  // for a single statement, begin-end is not needed : be it always, initial, if-else
     end
 endmodule
 
 
 // Always use <= (non blocking assignment) for sequential ckts -> better consistency, reduces errors
 // Always use = (blocking assignment) for combinational ckts -> better consistency , reduces errors
+// Note : when you are solving sequential ckt related questions on hdl bits website, you can see that reg isn't mentioned while declaring outputs, because the simulator is system verilog based, hence no issues, but you better mention reg explicitly
---
+++ hdlbits-solutions/03-circuits/02-sequential-logic/01-latches-and-flipflops/03_DFF_with_reset.v
@@ -0,0 +1,16 @@
+// Implementation of a 8-bit DFF with a synchronus reset setting it to 0
+module top_module(
+    input clk,
+    input reset,
+    input [7:0] d,
+    output reg [7:0] q);
+    always @(posedge clk) begin  
+        if (reset) begin    // this is synchronus reset, it works only on the +ve edge of clk
+            q <= 8'd0;      // can also be written as 8'b00000000 or 8'h00
+        end
+        else begin 
+            q <= d;
+        end
+    end
+endmodule
+
