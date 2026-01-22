4dfe2cc226098fd6fe9b5f50a580a05e87254347
sriramryali <sriramryali01@gmail.com>
2026-01-22 22:07:04 +0530

    initial commit

--- hdlbits-solutions/03-circuits/02-sequential-logic/01-latches-and-flipflops/18_dual_edge_triggered_flipflop.v
+++ hdlbits-solutions/03-circuits/02-sequential-logic/01-latches-and-flipflops/18_dual_edge_triggered_flipflop.v
@@ -1,1 +1,14 @@
-// 
+// Implementation of a dual edge triggered FF
+module top_module(
+    input clk,
+    input d,
+    output q); 
+    reg q_pos, q_neg;                  // always @(posedge clk or negedge clk) is not allowed in verilog
+    always @(posedge clk) begin        // this is for +ve edge of clk
+        q_pos <= d;                    // creating two variables to store the values at +ve ad -ve edges
+    end
+    always @(posedge clk) begin        // this is for -ve edge of clk
+        q_neg <= d;
+    end
+    assign q = clk ? q_pos : q_neg;    // assigning values to q based on clk 
+endmodule
